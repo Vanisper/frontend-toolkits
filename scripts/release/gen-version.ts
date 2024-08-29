40d4d4ecaf2666b04dd8cfd721f9cc8406ec9c2f
Vanisper <273266469@qq.com>
2024-08-30 03:36:31 +0800

    ci(gen-version.ts): 👷 版本生成文本内容改为双引号字符串，且以分号结尾

--- scripts/release/gen-version.ts
+++ scripts/release/gen-version.ts
@@ -7,6 +7,6 @@ import { mainRoot } from '../utils/paths'
 export async function genVersion(version?: string) {
   if (version) {
     consola.info(`Version: ${version}`)
-    await writeFile(path.resolve(mainRoot, 'version.ts'), `export const version = '${version}'\n`)
+    await writeFile(path.resolve(mainRoot, 'version.ts'), `export const version = "${version}";\n`)
   }
 }
